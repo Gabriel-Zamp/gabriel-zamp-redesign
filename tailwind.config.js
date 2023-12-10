848451d351eb38447a9468f7ddb7c6fbf462eb1b
gabrielzamp <gzampierimkt@gmail.com>
2023-12-10 17:41:24 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        syne: ['Syne', 'sans'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
