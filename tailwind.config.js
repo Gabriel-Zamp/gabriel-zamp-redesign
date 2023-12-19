0788fe77dee8851c4e93e70435d36479f3aed92b
gabrielzamp <gzampierimkt@gmail.com>
2023-12-18 22:57:28 -0300

    site responsivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,37 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
 
 module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        syne: ['Syne', 'sans'],
+        syne: ["Syne", "sans"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+module.exports = {
+  mode: "jit",
+  theme: {
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: "1rem",
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1496px",
       },
     },
   },
